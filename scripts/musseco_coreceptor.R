75ef19cdc88fe7a45ec835fa4654720c94bdc611
vinibfranc <vinibfranc@gmail.com>
2025-02-10 14:48:12 +0000

    env and V3 trees only

--- scripts/musseco_coreceptor.R
+++ scripts/musseco_coreceptor.R
@@ -17,14 +17,16 @@ load_tree_adjust_headers <- function(path_tr, pred_df) {
 }
 
 # load trees (change path if needed)
-timetr_gag <- load_tree_adjust_headers("data/timetr_mlscluster_popart_gag.rds", wp_nt_df)
-timetr_pol <- load_tree_adjust_headers("data/timetr2_mlscluster_popart_pol.rds", wp_nt_df)
 timetr_v3 <- load_tree_adjust_headers("data/timetr2_mlscluster_popart_v3.rds", wp_nt_df)
+timetr_v3$adjusted.mean.rate; timetr_v3$timeOfMRCA
+# [1] 0.003571885
+# [1] 1989.418
 timetr_env <- load_tree_adjust_headers("data/timetr_mlscluster_popart_env.rds", wp_nt_df)
-timetr_wgs <- load_tree_adjust_headers("data/timetr2_mlscluster_popart_wgs.rds", wp_nt_df)
+timetr_env$adjusted.mean.rate; timetr_env$timeOfMRCA
+# [1] 0.003179319
+# [1] 1972.893
 
-common_tips <- Reduce(intersect, list(timetr_gag$tip.label, timetr_pol$tip.label, timetr_v3$tip.label,
-                                      timetr_env$tip.label, timetr_wgs$tip.label)) #3940 (now 3880)
+common_tips <- Reduce(intersect, list(timetr_v3$tip.label,timetr_env$tip.label)) #4056
 
 # prepare isvariant vector (X4=T and R5=F)
 prepare_isvariant_vector <- function(tr, tips_keep) {
@@ -36,13 +38,8 @@ prepare_isvariant_vector <- function(tr, tips_keep) {
  isvariant
 }
 
-isvariant_gag <- prepare_isvariant_vector(timetr_gag, common_tips)
-isvariant_pol <- prepare_isvariant_vector(timetr_pol, common_tips)
-# v3
-isvariant_v3_1 <- prepare_isvariant_vector(timetr_v3, common_tips) #3880
-isvariant_v3_2 <- prepare_isvariant_vector(timetr_v3, timetr_v3$tip.label) #4095
+isvariant_v3 <- prepare_isvariant_vector(timetr_v3, common_tips)
 isvariant_env <- prepare_isvariant_vector(timetr_env, common_tips)
-isvariant_wgs <- prepare_isvariant_vector(timetr_wgs, common_tips)
 
 # make sure only common tips included
 keep_only_common_tips <- function(tr, c_tips) {
@@ -50,67 +47,36 @@ keep_only_common_tips <- function(tr, c_tips) {
  tr
 }
 
-timetr_gag <- keep_only_common_tips(timetr_gag, common_tips)
-timetr_pol <- keep_only_common_tips(timetr_pol, common_tips)
-# v3
-timetr_v3_1 <- keep_only_common_tips(timetr_v3, common_tips)
-timetr_v3_2 <- keep_only_common_tips(timetr_v3, timetr_v3$tip.label)
-
+timetr_v3 <- keep_only_common_tips(timetr_v3, common_tips)
+timetr_v3$adjusted.mean.rate; timetr_v3$timeOfMRCA
 timetr_env <- keep_only_common_tips(timetr_env, common_tips)
-timetr_wgs <- keep_only_common_tips(timetr_wgs, common_tips)
+timetr_env$adjusted.mean.rate; timetr_env$timeOfMRCA
 
 gamma <- 1/10.2
 
-NCPU <- 1
+NCPU <- 3
 
 system("mkdir -p results/musseco_coreceptor")
 
-fb_gag <- fitbisseco(timetr_gag, isvariant_gag, Tg=1/gamma, mu=timetr_gag$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
-                     mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_gag, "results/musseco_coreceptor/fb_gag.rds") # tau~76
-
-fb_pol <- fitbisseco(timetr_pol, isvariant_pol, Tg=1/gamma, mu=timetr_pol$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
-                     mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_pol, "results/musseco_coreceptor/fb_pol.rds") # tau~36 (prev 10)
-
 #v3
-fb_v3_1 <- fitbisseco(timetr_v3_1, isvariant_v3_1, Tg=1/gamma, mu=timetr_v3_1$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
+fb_v3 <- fitbisseco(timetr_v3, isvariant_v3, Tg=1/gamma, mu=timetr_v3$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
                       mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_v3_1, "results/musseco_coreceptor/fb_v3_1.rds")
-
-fb_v3_2 <- fitbisseco(timetr_v3_2, isvariant_v3_2, Tg=1/gamma, mu=timetr_v3_2$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
-                      mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_v3_2, "results/musseco_coreceptor/fb_v3_2.rds")
+saveRDS(fb_v3, "results/musseco_coreceptor/fb_v3.rds") #tau~13
 
 fb_env <- fitbisseco(timetr_env, isvariant_env, Tg=1/gamma, mu=timetr_env$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
                      mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_env, "results/musseco_coreceptor/fb_env.rds") # tau~16
+saveRDS(fb_env, "results/musseco_coreceptor/fb_env.rds") #tau~21
 
-fb_wgs <- fitbisseco(timetr_wgs, isvariant_wgs, Tg=1/gamma, mu=timetr_wgs$adjusted.mean.rate, Net=NULL, theta0=log(c(15, .95, 1)),
-                     mlesky_parms = list(tau = NULL, tau_lower = .1, tau_upper = 1e7, ncpu = NCPU, model = 1 ) )
-saveRDS(fb_wgs, "results/musseco_coreceptor/fb_wgs.rds") # tau~22 (prev 37)
-
-mus <- c(timetr_gag$adjusted.mean.rate, timetr_pol$adjusted.mean.rate, timetr_env$adjusted.mean.rate, 
-         timetr_v3$adjusted.mean.rate, timetr_wgs$adjusted.mean.rate)
+mus <- c(timetr_v3$adjusted.mean.rate, timetr_env$adjusted.mean.rate)
 
 RES_MUS_PATH <- "results/musseco_coreceptor"
-# read results
-fb_gag <- readRDS(glue("{RES_MUS_PATH}/fb_gag.rds"))
-fb_gag
-
-fb_pol <- readRDS(glue("{RES_MUS_PATH}/fb_pol.rds"))
-fb_pol
 
 # V3 loop
-fb_v3_1 <- readRDS(glue("{RES_MUS_PATH}/fb_v3_1.rds"))
-fb_v3_1
-# TODO other
+fb_v3 <- readRDS(glue("{RES_MUS_PATH}/fb_v3.rds"))
+fb_v3
 
 fb_env <- readRDS(glue("{RES_MUS_PATH}/fb_env.rds"))
 fb_env
-
-fb_wgs <- readRDS(glue("{RES_MUS_PATH}/fb_wgs.rds"))
-fb_wgs
 
 get_CI <- function(x, id_val){
  stopifnot( inherits(x, 'bissecofit' ))
@@ -127,34 +93,27 @@ get_CI <- function(x, id_val){
 }
 
 # combine all alpha and omega estimates
-fb_ci_gag <- get_CI(fb_gag,"gag")
-fb_ci_pol <- get_CI(fb_pol,"pol")
-# v3
-fb_ci_v3_1 <- get_CI(fb_v3_1,"V3")
-# TODO other
+fb_ci_v3 <- get_CI(fb_v3,"V3")
 fb_ci_env <- get_CI(fb_env,"env")
-fb_ci_wgs <- get_CI(fb_wgs,"WGS")
 
-order_genes <- c("gag","pol","V3","env","WGS") #V3
-fb_all <- rbind(fb_ci_gag, fb_ci_pol, fb_ci_v3_1, fb_ci_env, fb_ci_wgs)
+order_genes <- c("V3","env")
+fb_all <- rbind(fb_ci_v3, fb_ci_env)
 fb_all$id <- factor(fb_all$id, labels=order_genes, levels=order_genes)
 fb_all <- fb_all[fb_all$var != "yscale",]
 
 plot_alpha_omega <- function(fbdf) {
  pl <- ggplot( fbdf, aes( x = id, y = median_estimate, color=id)) + 
-  geom_pointrange(aes(ymin = `2.5%`, ymax = `97.5%`)) + labs(x='Genomic region of time-scaled tree', y='Estimate') + 
+  geom_pointrange(aes(ymin = `2.5%`, ymax = `97.5%`)) + labs(x='Genomic region of timetree', y='Estimate (95% CI)') + #time-scaled tree
   scale_color_manual(values=cbb_palette_genes, name="Genomic region of \ntime-scaled tree") + 
   facet_wrap(~var, scales="free_y") + 
   theme_bw() + common_plot_config + theme(legend.position = "none")
  pl
 }
 
-# TODO change omega y-axis? & add symbols of alpha and omega?
 plot_ao <- plot_alpha_omega(fb_all)
-ggsave(filename="results/figs/fig_alpha_omega_genes.pdf", plot=plot_ao, units="cm", width=15, height=12, dpi=300)
+#ggsave(filename="results/figs/fig_alpha_omega_genes.pdf", plot=plot_ao, units="cm", width=15, height=12, dpi=300)
 
 # combine all Ne estimates
-
 attrib_rename_ne_cols <- function(ne_obj, id_val) {
  ne <- as.data.frame(ne_obj$Net)
  ne$id <- id_val
@@ -162,25 +121,51 @@ attrib_rename_ne_cols <- function(ne_obj, id_val) {
  ne
 }
 
-ne_gag <- attrib_rename_ne_cols(fb_gag,"gag")
-ne_pol <- attrib_rename_ne_cols(fb_pol,"pol")
-# V3 loop
-ne_v3_1 <- attrib_rename_ne_cols(fb_v3_1,"V3")
-# TODO other
+ne_v3 <- attrib_rename_ne_cols(fb_v3,"V3")
 ne_env <- attrib_rename_ne_cols(fb_env,"env")
-ne_wgs <- attrib_rename_ne_cols(fb_wgs,"WGS")
 
-ne_all <- rbind(ne_gag, ne_pol, ne_v3_1, ne_env, ne_wgs)
+ne_all <- rbind(ne_v3, ne_env)
 ne_all$id <- factor(ne_all$id, labels=order_genes, levels=order_genes)
 
 plot_ne <- function(nedf,logy=T) {
  pl <- ggplot( nedf, aes( x = time, y = Ne, color=id)) + geom_line(size=0.75) + labs(y='Effective population size', x='Time since the estimated root date') + 
   scale_x_continuous(limits=c(0,70), breaks=c(seq(from=0,to=70,by=10))) +
-  scale_color_manual(values=cbb_palette_genes, name="Genomic region of \ntime-scaled tree") + 
-  theme_bw() + common_plot_config #ggplot2::geom_ribbon( ggplot2::aes( ymin = nelb, ymax = neub), fill = 'blue', alpha = .2)
+  scale_color_manual(values=cbb_palette_genes, name="Genomic region of \ntime-scaled tree") +
+  theme_bw() + common_plot_config
  if (logy) pl <- pl + scale_y_continuous(trans = 'log10', breaks = c(10^1, 10^2, 10^3, 10^4, 10^5, 10^6), labels = scales::trans_format("log10", scales::math_format(10^.x))) #scale_y_log10()
  pl
 }
 
-plot_ne_all <- plot_ne(ne_all)
-ggsave(filename="results/figs/supp_fig_ne_genes.pdf", plot=plot_ne_all, units="cm", width=18, height=15, dpi=300)
+plot_ne_all_no_ci <- plot_ne(ne_all)
+#ggsave(filename="results/figs/supp_fig_ne_genes.pdf", plot=plot_ne_all, units="cm", width=18, height=15, dpi=300)
+
+# parboot to get CIs on Ne
+pboot_v3 <- mlesky::parboot(fb_v3$mleskyfit, nrep=100, ncpu = NCPU, dd=F)
+saveRDS(pboot_v3, glue("{RES_MUS_PATH}/pboot_v3.rds"))
+
+pboot_env <- mlesky::parboot(fb_env$mleskyfit, nrep=100, ncpu = NCPU, dd=F)
+saveRDS(pboot_env, glue("{RES_MUS_PATH}/pboot_env.rds"))
+
+pboot_v3 <- readRDS(glue("{RES_MUS_PATH}/pboot_v3.rds"))
+pboot_v3$ne_ci <- as.data.frame(pboot_v3$ne_ci)
+pboot_v3$ne_ci$id <- "V3"
+pboot_v3$ne_ci$time <- pboot_v3$time
+pboot_env <- readRDS(glue("{RES_MUS_PATH}/pboot_env.rds"))
+pboot_env$ne_ci <- as.data.frame(pboot_env$ne_ci)
+pboot_env$ne_ci$id <- "env"
+pboot_env$ne_ci$time <- pboot_env$time
+
+# plot both together with CIs
+ne_ci_all <- rbind(pboot_v3$ne_ci, pboot_env$ne_ci)
+ne_ci_all$id <- factor(ne_ci_all$id, labels=order_genes, levels=order_genes)
+
+pl_ne <- ggplot( ne_ci_all, aes( x = time, y = ne, color=id, fill=id)) + geom_line(size=0.75) + labs(y='Effective population size (95% CI)', x='Time since the estimated root date') + 
+ scale_x_continuous(limits=c(0,50), breaks=c(seq(from=0,to=50,by=10))) +
+ scale_color_manual(values=cbb_palette_genes, name="Timetree") + #name="Genomic region of timetree"
+ scale_fill_manual(values=cbb_palette_genes, name="Timetree") +
+ geom_ribbon( aes( ymin = nelb, ymax = neub), alpha = .2) + 
+ scale_y_continuous(trans = 'log10', breaks = c(10^1, 10^2, 10^3, 10^4, 10^5, 10^6, 10^7), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+ theme_bw() + theme(legend.position = c(0.7,0.2)) + common_plot_config
+
+fig_ao_ne <- plot_grid(plot_ao, pl_ne, nrow = 1, ncol=2, labels=c("A","B"), rel_widths = c(3.5,5), label_size = 7) #align = "hv",
+ggsave(filename="results/figs/fig_alpha_omega_ne_coreceptor.pdf", plot=fig_ao_ne, units="cm", width=15, height=10, dpi=300)
